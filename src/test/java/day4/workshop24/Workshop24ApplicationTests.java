255293f546b89946a20ef9942ab0111f1ed1e750
teo_hong_zhang <thehotzebra@gmail.com>
2023-02-02 14:46:47 +0800

    first

---
+++ src/test/java/day4/workshop24/Workshop24ApplicationTests.java
@@ -0,0 +1,13 @@
+package day4.workshop24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
